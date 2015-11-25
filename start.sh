0dd252e51d983829346e615459651d6cc9eff5f2
Ayush Jha <ayushjha6@gmail.com>
2015-11-25 11:39:36 +0545

    modified start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,13 @@ if [[ $EUID -ne 0 ]]; then
   pip install --user -r requirements.txt
   ./manage.py makemigrations
   ./manage.py migrate
-  ./manage.py runserver_plus
+  ./manage.py runserver_plus &>> serverlog &
+  sleep 10
+  if which xdg-open > /dev/null;then
+    xdg-open http://localhost:8000 &> browserlog
+  elif which gnome-open > /dev/null; then
+    gnome-open http://localhost:8000 &> browserlog
+  fi
 else
   echo "Root is for the dirt! Take me somewhere higher!"
 fi
