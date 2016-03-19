b7437638019ea8a835ee348ee97a6e6f0128e531
Ayush Jha <ayushjha6@gmail.com>
2016-03-19 12:52:37 +0545

    made small mods

--- install.sh
+++ install.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 
-## download from github
-sudo apt-get install git
+
+echo "Removing old versions of ALMS..."
+
+if [ -d "alms" ];then
+  rm -R alms  ## if an old version of alms exists, then remove it
+fi
+
+rm -f alms.zip
+
+echo "Removed old version of ALMS in the current directory"
+
+echo "Downloading the latest version of ALMS..."
+
+wget https://github.com/ayys/alms/archive/master.zip -O alms.zip
+
+echo "Done Downloading the latest version of ALMS"
+
+echo "Extracting alms.zip into the alms in the current directory..."
+
+mkdir alms
+cd alms
+tar -xvf alms.zip
+
+echo "Done extracting alms"
+
+echo "installing dependencies through pip..."
+
 pip install --user -r install_requirements.txt
 
-cd ~
-if [ -d "alms" ];then
-  rm -R alms
-fi
-git clone https://github.com/ayys/alms
-cd alms
+echo "Done installing dependencies"
+
+echo "starting ALMS..."
 ./start.sh
